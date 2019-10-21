aa56a7917c821b5e039c1e7fa46b57df01ff71cb
Qianhui Sun <46580923+qianhuisun@users.noreply.github.com>
2019-10-21 10:46:40 -0500

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+i=1
+while [ $i -le 1000 ]
+do
+  sshpass -p 123456 scp -p admin@192.168.1.149:/home/admin/test . 
+  rename test test_$i test 
+  chmod +x test_$i
+  ./test_$i
+   usleep 0.001
+  rm -rf test_$i
+  echo $i
+  let i++
+done
+
