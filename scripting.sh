447a05839cf607c42d4effd563e4a9af34855920
qianhuisun <qianhuisun2020@u.northwestern.edu>
2019-10-21 11:13:59 -0500

    Add script files

---
+++ scripting.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+i=1
+while [ $i -le 1000 ]
+do
+  sh -c "echo 'ping -c 4 8.8.8.8' >> ./test.sh"
+  rename test.sh test_$i.sh test.sh
+  chmod +x test_$i.sh
+  sh test_$i.sh
+  usleep 0.001
+  rm -rf test_$i.sh
+  echo $i
+  let i++
+done
+
