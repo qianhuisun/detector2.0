a2ccb42720c43a3b8bd83c697054815576a69b72
Fan Hu <aaron97@foxmail.com>
2019-12-11 18:28:51 -0600

    test lttng2spade download_execution.sh

--- download_execution.sh
+++ download_execution.sh
@@ -2,11 +2,10 @@
 i=1
 while [ $i -le 10 ]
 do
-  wget https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1154/echo-art-fish.sh
-  rename echo-art-fish.sh test_$i echo-art-fish.sh 
+  wget -O test_$i https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1154/echo-art-fish.sh
   chmod +x test_$i
   ./test_$i
-  usleep 0.001
+  # usleep 0.001
   rm -rf test_$i
   echo $i
   let i++
