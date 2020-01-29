61b34ea98b3e89df8fa15afdb98c447fee50b719
qianhuisun <qianhuisun2020@u.northwestern.edu>
2020-01-29 16:12:45 -0600

    fix script:

--- linux_socket/tcp_server.py
+++ linux_socket/tcp_server.py
@@ -15,40 +15,21 @@ def createServer():
     print("Server is listening ...")
     serverSocket.settimeout(10)
 
-    inputs = [serverSocket]
+    connectSocket, addr = serverSocket.accept()
 
-    while inputs:
-        
-        input("Enter to continue > ")
-
-        readable, writable, exceptional = select(inputs, [], inputs)
-
-        for s in exceptional:
-            inputs.remove(s)
-            s.close()
-        
-        for s in readable:
-            if s is serverSocket:
-                connectSocket, addr = s.accept()
-                print("Got client addr: " + str(addr))
-                connectSocket.setblocking(0)
-                inputs.append(connectSocket)
-            else:
-                inputs.remove(s)
-                s.setblocking(1)
-                listening = True
-                try:
-                    while listening:
-                        _bytes = s.recv(1024)
-                        _data = _bytes.decode()
-                        print("-" * 20)
-                        print(_data)
-                        if _data == "DONE":
-                            listening = False
-                    s.close()
-                except Exception as e:
-                    print(e)
-                    s.close()
+    try:
+        _bytes = connectSocket.recv(1024)
+        _data = _bytes.decode()
+        print("-" * 20)
+        print(_data)
+        _bytes = connectSocket.recv(1024)
+        _data = _bytes.decode()
+        print("-" * 20)
+        print(_data)
+        connectSocket.close()
+    except Exception as e:
+        print(e)
+        connectSocket.close()
 
     print("-" * 20)
     print("Server is shutting dowen ...")
