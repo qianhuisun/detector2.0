fe2a42dbb6ad9d73719f2c855a335e24b51015c5
qianhuisun <qianhuisun2020@u.northwestern.edu>
2020-01-29 17:51:33 -0600

    edit fan's server

--- fan/server.py
+++ fan/server.py
@@ -5,11 +5,8 @@ import os
 server_address = './uds_socket'
 
 # Make sure the socket does not already exist
-try:
-    os.unlink(server_address)
-except OSError:
-    if os.path.exists(server_address):
-        raise
+if os.path.exists(server_address):
+    os.remove(server_address)
 
 # Create a UDS socket
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
